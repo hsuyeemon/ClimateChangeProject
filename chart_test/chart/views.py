0a8a07fa1fc3d3788e1e4875649bce9ae77a0cac
DESKTOP-6LKLSI5\user <hsuyeemon.1@gmail.com>
2019-07-15 22:12:16 +0630

    fusion chart testing

---
+++ chart_test/chart/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Include the `fusioncharts.py` file which has required functions to embed the charts in html page
+from .fusioncharts import FusionCharts
+
+# Loading Data from a Static JSON String
+# The `chart` method is defined to load chart data from an JSON string.
+
+def chart(request):
+    # Create an object for the column2d chart using the FusionCharts class constructor
+    column2d = FusionCharts("column2d", "ex2" , "100%", "400", "chart-1", "json",
+        # The data is passed as a string in the `dataSource` as parameter.
+                        """{
+                            "chart": {
+                                "caption": "Recommended Portfolio Split",
+                                "subCaption" : "For a net-worth of $1M",
+                                "showValues":"1",
+                                "showPercentInTooltip" : "0",
+                                "numberPrefix" : "$",
+                                "enableMultiSlicing":"1",
+                                "theme": "fusion"
+                            },
+                            "data": [{
+                                "label": "Equity",
+                                "value": "300000"
+                            }, {
+                                "label": "Debt",
+                                "value": "230000"
+                            }, {
+                                "label": "Bullion",
+                                "value": "180000"
+                            }, {
+                                "label": "Real-estate",
+                                "value": "270000"
+                            }, {
+                                "label": "Insurance",
+                                "value": "20000"
+                            }]
+                           }""") 
+    # returning complete JavaScript and HTML code, which is used to generate chart in the browsers.
+    return render(request, 'index.html', {'output' : column2d.render()})
